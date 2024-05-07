45726928300250beed3f45fb49590e3b6da1c73e
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2024-05-07 19:10:33 -0300

    feat: add password authentication

--- database.py
+++ database.py
@@ -17,13 +17,20 @@ class Database():
                 database = self.database
             )
             print("Conexão bem sucedida!")
-            self.cursor = self.cnx.cursor()
+            self.cursor = self.cnx.cursor(buffered=True)
             return self.cnx
         except mysql.connector.Error as err:
             print(f"Falha na conexão! {err}")
             return None
         
     def add_user_to_database(self, email, password, role_id):
-        sql = "INSERT INTO tb_user(user_email, user_password, role_id) VALUES (%s, %s, %d)" # Ainda é necessário a implementação de uma janela com a opção de seleção de cargos.
-        self.cursor.execute(sql, (email, password, role_id))
+        sql = ("INSERT INTO tb_user(user_email, user_password, role_id) VALUES (%s, %s, %s)" % email, password, role_id) # Ainda é necessário a implementação de uma janela com a opção de seleção de cargos.
+        self.cursor.execute(sql)
         self.cnx.commit()
+
+    def get_password_from_database(self, email):
+        sql = ("SELECT user_password FROM tb_user WHERE user_email='%s'" % email)
+        self.cursor.execute(sql)
+        self.cnx.commit()
+        password = self.cursor.fetchone()[0]
+        return password
--- window.py
+++ window.py
@@ -33,8 +33,8 @@ class Window():
         self.password_input = customtkinter.CTkEntry(self.screen, width=200, height=30, placeholder_text="Senha", show="*")
         self.password_input.place(x=WIDTH/2, y=HEIGHT/2, anchor="center")
 
-    def create_button(self, text):
-        login_button = customtkinter.CTkButton(self.screen, width=200, height=30, text=text, fg_color=blue_link_color, hover_color=blue_link_hover_color)
+    def create_button(self, text, command):
+        login_button = customtkinter.CTkButton(self.screen, width=200, height=30, text=text, fg_color=blue_link_color, hover_color=blue_link_hover_color, command=command)
         login_button.place(x=WIDTH/2, y=HEIGHT/2 + 40, anchor="center")
 
     def create_login_link(self):
@@ -44,6 +44,10 @@ class Window():
         login_link_button = customtkinter.CTkButton(self.screen, width=0, height=0, text="Clique aqui", command=self.create_login_widgets, text_color=blue_link_color, fg_color="transparent", hover=False, cursor="hand2")
         login_link_button.place(x=WIDTH/2 + 50, y=HEIGHT/2 + 80, anchor="center")
 
+    def clear_window(self):
+        for widget in self.screen.winfo_children():
+            widget.destroy()
+
     def get_email(self):
         email = self.email_input.get()
         return email
@@ -51,10 +55,12 @@ class Window():
     def get_password(self):
         password = self.password_input.get()
         return sha256(password.encode("utf-8")).hexdigest()
-
-    def clear_window(self):
-        for widget in self.screen.winfo_children():
-            widget.destroy()
+    
+    def check_user(self):
+        if self.get_password() == database.get_password_from_database(self.get_email()):
+            print("Login feito com sucesso!")
+        else:
+            print("Email ou senha incorretos!")
 
     def create_login_widgets(self):
         self.clear_window()
@@ -62,7 +68,7 @@ class Window():
 
         self.create_email_input()
         self.create_password_input()
-        self.create_button("Login")
+        self.create_button("Login", self.check_user)
         self.create_login_link()
         self.screen.mainloop()
 
