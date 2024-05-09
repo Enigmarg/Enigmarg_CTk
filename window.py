711dda32edf2d65940e9f17608f0cb0687c0abc4
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2024-05-09 20:53:37 -0300

    fix: reduce functions and avoid inputs conflicts

--- window.py
+++ window.py
@@ -26,25 +26,27 @@ class Window():
 
         self.screen.geometry("%dx%d+%d+%d" % (WIDTH, HEIGHT, x, y))
 
-    def create_email_input(self, x=0, y=0):
-        self.email_input = customtkinter.CTkEntry(self.screen, width=200, height=30, placeholder_text="E-mail")
-        self.email_input.place(x=x, y=y, anchor="center")
-
-    def create_password_input(self):
-        self.password_input = customtkinter.CTkEntry(self.screen, width=200, height=30, placeholder_text="Senha", show="*")
-        self.password_input.place(x=WIDTH/2, y=HEIGHT/2, anchor="center")
-
-    def create_button(self, text, command):
-        login_button = customtkinter.CTkButton(self.screen, width=200, height=30, text=text, fg_color=blue_link_color, hover_color=blue_link_hover_color, command=command)
-        login_button.place(x=WIDTH/2, y=HEIGHT/2 + 40, anchor="center")
-
-    def link_forgot_password(self, command):
-        login_link_button = customtkinter.CTkButton(self.screen, width=0, height=0, text="Esqueceu a senha?", command=command, text_color=blue_link_color, fg_color="transparent", hover=False, cursor="hand2")
-        login_link_button.place(x=WIDTH/2, y=HEIGHT/2 + 80, anchor="center")
-
     def clear_window(self):
         for widget in self.screen.winfo_children():
             widget.destroy()
+
+    def create_login_screen(self):
+        self.clear_window()
+        self.screen.title("Login")
+
+        self.email_input = customtkinter.CTkEntry(self.screen, width=200, height=30, placeholder_text="E-mail")
+        self.email_input.place(x = WIDTH/2, y = HEIGHT/2-40, anchor="center")
+
+        self.password_input = customtkinter.CTkEntry(self.screen, width=200, height=30, placeholder_text="Senha", show="*")
+        self.password_input.place(x=WIDTH/2, y=HEIGHT/2, anchor="center")
+
+        login_button = customtkinter.CTkButton(self.screen, width=200, height=30, text="Login", fg_color=blue_link_color, hover_color=blue_link_hover_color, command=None)
+        login_button.place(x=WIDTH/2, y=HEIGHT/2 + 40, anchor="center")
+
+        login_link_button = customtkinter.CTkButton(self.screen, width=0, height=0, text="Esqueceu a senha?", command=None, text_color=blue_link_color, fg_color="transparent", hover=False, cursor="hand2")
+        login_link_button.place(x=WIDTH/2, y=HEIGHT/2 + 80, anchor="center")
+
+        self.screen.mainloop()
 
     def get_email(self):
         email = self.email_input.get()
@@ -62,15 +64,5 @@ class Window():
             print("Email ou senha incorretos!")
             messagebox.showerror("Erro", "Email ou senha incorretos")
 
-    def create_login_widgets(self):
-        self.clear_window()
-        self.screen.title("Login")
-
-        self.create_email_input(x=WIDTH/2, y=HEIGHT/2-40)
-        self.create_password_input()
-        self.create_button("Login", self.check_user)
-        self.link_forgot_password(None)
-        self.screen.mainloop()
-
 if __name__ == "__main__":
-    Window().create_login_widgets()
+    Window().create_login_screen()
