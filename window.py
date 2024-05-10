bf7a226c08a2aba5ddc5ba96252a275feda9ac9e
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2024-05-10 09:37:49 -0300

    fix: put size variables in the Login class

--- window.py
+++ window.py
@@ -6,14 +6,14 @@ from hashlib import sha256
 database = Database()
 database.connect()
 
-WIDTH = 380
-HEIGHT = 450
-
 blue_link_color = "#0066CC"
 blue_link_hover_color = "#0059B3"
 
-class Window():
+class Login():
+
     def __init__(self):
+        self.WIDTH = 380
+        self.HEIGHT = 450
         self.screen = customtkinter.CTk()
         customtkinter.set_appearance_mode("dark")
         self.screen.resizable(False, False)
@@ -21,10 +21,10 @@ class Window():
         # Centers the window
         screen_width = self.screen.winfo_screenwidth()
         screen_height = self.screen.winfo_screenheight()
-        x = (screen_width / 2) - (WIDTH / 2)
-        y = (screen_height / 2) - (HEIGHT / 2)
+        x = (screen_width / 2) - (self.WIDTH / 2)
+        y = (screen_height / 2) -  ( self.HEIGHT / 2)
 
-        self.screen.geometry("%dx%d+%d+%d" % (WIDTH, HEIGHT, x, y))
+        self.screen.geometry("%dx%d+%d+%d" % (self.WIDTH, self.HEIGHT, x, y))
 
     def clear_window(self):
         for widget in self.screen.winfo_children():
@@ -35,16 +35,16 @@ class Window():
         self.screen.title("Login")
 
         self.email_input = customtkinter.CTkEntry(self.screen, width=200, height=30, placeholder_text="E-mail")
-        self.email_input.place(x = WIDTH/2, y = HEIGHT/2-40, anchor="center")
+        self.email_input.place(x = self.WIDTH/2, y = self.HEIGHT/2-40, anchor="center")
 
         self.password_input = customtkinter.CTkEntry(self.screen, width=200, height=30, placeholder_text="Senha", show="*")
-        self.password_input.place(x=WIDTH/2, y=HEIGHT/2, anchor="center")
+        self.password_input.place(x = self.WIDTH/2, y = self.HEIGHT/2, anchor="center")
 
         login_button = customtkinter.CTkButton(self.screen, width=200, height=30, text="Login", fg_color=blue_link_color, hover_color=blue_link_hover_color, command=self.check_user)
-        login_button.place(x=WIDTH/2, y=HEIGHT/2 + 40, anchor="center")
+        login_button.place(x = self.WIDTH/2, y = self.HEIGHT/2 + 40, anchor="center")
 
         link_forgot_password = customtkinter.CTkButton(self.screen, width=0, height=0, text="Esqueceu a senha?", command=self.create_forgot_password_screen, text_color=blue_link_color, fg_color="transparent", hover=False, cursor="hand2")
-        link_forgot_password.place(x=WIDTH/2, y=HEIGHT/2 + 80, anchor="center")
+        link_forgot_password.place(x = self.WIDTH/2, y = self.HEIGHT/2 + 80, anchor="center")
 
         self.screen.mainloop()
 
@@ -53,13 +53,13 @@ class Window():
         self.screen.title("Login")
 
         self.email_input = customtkinter.CTkEntry(self.screen, width=200, height=30, placeholder_text="E-mail")
-        self.email_input.place(x = WIDTH/2, y = HEIGHT/2-20, anchor="center")
+        self.email_input.place(x = self.WIDTH/2, y = self.HEIGHT/2-20, anchor="center")
 
         send_button = customtkinter.CTkButton(self.screen, width=200, height=30, text="Enviar", fg_color=blue_link_color, hover_color=blue_link_hover_color, command=None)
-        send_button.place(x=WIDTH/2, y=HEIGHT/2 + 20, anchor="center")
+        send_button.place(x = self.WIDTH/2, y = self.HEIGHT/2 + 20, anchor="center")
 
         link_return = customtkinter.CTkButton(self.screen, width=0, height=0, text="Voltar", command=self.create_login_screen, text_color=blue_link_color, fg_color="transparent", hover=False, cursor="hand2")
-        link_return.place(x=WIDTH/2, y=HEIGHT/2 + 60, anchor="center")
+        link_return.place(x = self.WIDTH/2, y = self.HEIGHT/2 + 60, anchor="center")
 
         self.screen.mainloop()
 
@@ -80,4 +80,4 @@ class Window():
             messagebox.showerror("Erro", "Email ou senha incorretos")
 
 if __name__ == "__main__":
-    Window().create_login_screen()
+    Login().create_login_screen()
