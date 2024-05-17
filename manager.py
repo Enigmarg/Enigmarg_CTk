2266d52e8c75c4c6202a625a33df59926cdcdc10
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2024-05-16 21:45:46 -0300

    feat: add buttons to questions manager screen

--- manager.py
+++ manager.py
@@ -163,6 +163,17 @@ class Manager():
         self.answer_option = customtkinter.CTkOptionMenu(tabView.tab("Questões"), values=["1", "2", "3", "4"], width=5, height=40, fg_color="gray25", button_color="gray25", hover=False)
         self.answer_option.place(relx=0.85, rely=0.1)
 
+        add_btn = customtkinter.CTkButton(tabView.tab("Questões"), text="Adicionar", width=170, height=40, corner_radius=50, font=BOLD_FONT, fg_color="royal blue", hover=False, command=self.add_user, cursor="hand2")
+        add_btn.place(relx=0.03, rely=0.47)
+
+        update_btn = customtkinter.CTkButton(tabView.tab("Questões"), text="Atualizar", width=170, height=40, corner_radius=50, font=BOLD_FONT, fg_color="royal blue", hover=False, command=self.update_user,  cursor="hand2")
+        update_btn.place(relx=0.28, rely=0.47)
+
+        delete_btn = customtkinter.CTkButton(tabView.tab("Questões"), text="Deletar", width=170, height=40, corner_radius=50, font=BOLD_FONT, fg_color="royal blue", hover=False, command=self.delete_user,  cursor="hand2")
+        delete_btn.place(relx=0.53, rely=0.47)
+
+        delete_btn = customtkinter.CTkButton(tabView.tab("Questões"), text="Limpar tudo", width=100, height=40, corner_radius=50, font=BOLD_FONT, text_color="royal blue", fg_color="transparent", border_width=1, border_color="royal blue", hover=False, command=self.delete_user,  cursor="hand2")
+        delete_btn.place(relx=0.82, rely=0.47)
 
         self.tree.bind("<ButtonRelease>", self.display_user)
         self.add_to_treeview()
