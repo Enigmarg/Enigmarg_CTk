5462c3eef41a68f01a974ebbe54daf82e13a456b
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2024-05-28 08:15:23 -0300

    feat: add some comments to database file

--- database.py
+++ database.py
@@ -12,6 +12,7 @@ class Database():
         self.database = os.getenv("DATABASE")
         # self.port = os.getenv("PORT")
 
+    # Tenta fazer a conexão com o banco de dados.
     def connect(self):
         try:
             self.cnx = mysql.connector.connect(
@@ -28,6 +29,7 @@ class Database():
             print(f"Falha na conexão! {err}")
             return None
         
+    # Pega e retorna a senha do usuário do banco de dados.
     def get_user_password(self, email:str):
         try:
             sql = ("SELECT user_password FROM tb_user WHERE user_email='%s'" % email)
@@ -38,6 +40,7 @@ class Database():
         except Exception as e:
             print(e)
             
+    # Pega e retorna todos os usuários do banco de dados.
     def get_all_users(self):
         sql = ("SELECT tb_user.user_email, role_name FROM tb_role INNER JOIN tb_user ON tb_user.role_id = tb_role.role_id WHERE role_name != 'Admin'")
         self.cursor.execute(sql)
@@ -45,11 +48,13 @@ class Database():
         self.cnx.commit()
         return users
         
+    # Adiciona um usuário ao banco de dados.
     def add_user(self, email:str, password:str, role:str):
         sql = ("INSERT INTO tb_user (user_email, user_password, role_id) SELECT '%s', '%s', role_id FROM tb_role WHERE tb_role.role_name = '%s'" % (email, password, role))
         self.cursor.execute(sql)
         self.cnx.commit()
 
+    # Atualiza um usuário no banco de dados.
     def update_user(self, email:str, password:str, role:str):
         if (email and password and role):
             sql = ("UPDATE tb_user JOIN tb_role ON tb_role.role_name = '%s' SET tb_user.user_email = '%s', tb_user.user_password = '%s', tb_user.role_id = tb_role.role_id WHERE tb_user.user_email = '%s'" % (role, email, password, email))
@@ -60,11 +65,13 @@ class Database():
             self.cursor.execute(sql)
             self.cnx.commit()  
 
+    # Deleta um usuário do banco de dados.
     def delete_user(self, email:str):
         sql = ("DELETE FROM tb_user WHERE user_email = '%s'" % email)
         self.cursor.execute(sql)
         self.cnx.commit()
 
+    # Pega e retorna todas as questões do banco de dados no formato JSON.
     def get_all_questions_json(self):
         sql = ("""
             SELECT JSON_OBJECT(
@@ -80,6 +87,7 @@ class Database():
         self.cnx.commit()
         return questions
     
+    # Adiciona uma questão ao banco de dados.
     def add_question(self, question:str, alter1:str, alter2:str, alter3:str, answer:str):
         self.cursor.execute("INSERT INTO tb_question (question_text) SELECT '%s'" % (question))
         question_id = self.cursor.lastrowid
@@ -87,13 +95,15 @@ class Database():
         self.cnx.commit()
         print("Questão adicionada!")
 
+    # Atualiza uma questão do banco de dados.
     def update_question(self, question_id:str, question:str, alter1:str, alter2:str, alter3:str, answer):
         self.cursor.execute("START TRANSACTION")
 
+        # Atualiza a pergunta da questão.
         sql1 = ("UPDATE tb_question SET question_text = '%s' WHERE question_id = %s" % (question, question_id))
         self.cursor.execute(sql1)
 
-        # Atualiza cada alternativa individualmente
+        # Atualiza cada alternativa individualmente.
         sql2 = ("""
             UPDATE tb_answer
             JOIN (
@@ -115,11 +125,13 @@ class Database():
         self.cursor.execute(sql2)
         self.cnx.commit()
 
+    # Deleta uma questão do banco de dados.
     def delete_question(self, question_id:int):
         sql = ("DELETE FROM tb_question WHERE question_id = '%s'" % (question_id))
         self.cursor.execute(sql)
         self.cnx.commit()
 
+    # Pega todas as questões do banco de dados.
     def get_all_questions(self):
         sql = ("""
             SELECT tb_question.question_id, tb_question.question_text AS pergunta,
@@ -134,6 +146,7 @@ class Database():
         self.cnx.commit()
         return questions
 
+    # Adiciona ou atualiza um usuário no ranking.
     def add_score(self, user_id:int, score:int):
         sql = ("SELECT * FROM tb_ranking WHERE user_id = %s" % (user_id))
         self.cursor.execute(sql)
