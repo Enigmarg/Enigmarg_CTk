b4f4bae9f678532b4b6e98f44414348e6d3e6eaf
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2024-05-28 16:42:59 -0300

    feat: add function and button to hide and view password in login screen

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 import customtkinter
+import tkinter as tk
 from tkinter import messagebox
 from database import Database
 from hashlib import sha256
@@ -22,16 +23,28 @@ class Login():
 
         self.screen.geometry("%dx%d+%d+%d" % (self.width, self.height, x, y))
 
+        self.enigmarg_logo = Image.open("resources/logo.png")
+        self.view_icon = tk.PhotoImage(file="resources/view.png")
+        self.hide_icon = tk.PhotoImage(file="resources/hide.png")
+
     def clear_screen(self):
         for widget in self.screen.winfo_children():
             widget.destroy()
+
+    def toggle_password(self):
+        if self.password_input.cget("show") == "":
+            self.password_input.configure(show="*")
+            self.toggle_btn.configure(image=self.hide_icon)
+        else:
+            self.password_input.configure(show="")
+            self.toggle_btn.configure(image=self.view_icon)
 
     def create_login_screen(self):
         self.clear_screen()
         self.screen.title("Login")
 
-        enigmarg_logo = customtkinter.CTkImage(light_image=Image.open("resources/logo.png"), dark_image=Image.open("resources/logo.png"), size=(185, 145))
-        logo_label = customtkinter.CTkLabel(self.screen,text="", image=enigmarg_logo)
+        enigmarg_logo = customtkinter.CTkImage(light_image=self.enigmarg_logo, dark_image=self.enigmarg_logo, size=(185, 145))
+        logo_label = customtkinter.CTkLabel(self.screen, text="", image=enigmarg_logo)
         logo_label.pack(pady=45)
 
         self.email_input = customtkinter.CTkEntry(self.screen, width=200, height=30, placeholder_text="E-mail", border_width=0)
@@ -42,6 +55,9 @@ class Login():
 
         login_btn = customtkinter.CTkButton(self.screen, width=200, height=30, text="Entrar", fg_color="royal blue", hover=False, command=self.check_user)
         login_btn.place(relx=0.5, rely=0.7, anchor="center")
+
+        self.toggle_btn = customtkinter.CTkButton(self.password_input, text="", width=0, height=0, image=self.hide_icon, fg_color="transparent", hover=False, command=self.toggle_password)
+        self.toggle_btn.place(relx=0.9, rely=0.5, anchor="center")
 
         forgot_password_btn = customtkinter.CTkButton(self.screen, width=0, height=0, text="Esqueceu a senha?", command=self.create_forgot_password_screen, text_color="white", fg_color="transparent", hover=False, cursor="hand2")
         forgot_password_btn.place(relx=0.5, rely=0.8, anchor="center")
