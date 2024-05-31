b6b5ff43f72766e1e99e450549660bdce9d987fb
Vitor Eiken <117394582+eikenn@users.noreply.github.com>
2024-05-31 16:05:24 -0300

    feat: add MIME format to send emails and update email text

--- login.py
+++ login.py
@@ -4,8 +4,10 @@ from tkinter import messagebox
 from database import Database
 from hashlib import sha256
 from PIL import Image
-import smtplib
 import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 class Login():
 
@@ -28,6 +30,12 @@ class Login():
         self.enigmarg_logo = Image.open("resources/logo.png")
         self.view_icon = tk.PhotoImage(file="resources/view.png")
         self.hide_icon = tk.PhotoImage(file="resources/hide.png")
+
+        self.smtp_server = os.getenv("SMTP_SERVER")
+        self.smtp_port = os.getenv("SMTP_PORT")
+        self.sender_email = os.getenv("SENDER_EMAIL")
+        self.sender_password = os.getenv("SENDER_PASSWORD")
+        self.receiver_email = os.getenv("RECEIVER_EMAIL")
 
     def clear_screen(self):
         for widget in self.screen.winfo_children():
@@ -57,18 +65,37 @@ class Login():
             print("Email ou senha inválidos.")
             messagebox.showerror("Erro", "Email ou senha inválidos.")
         
-    def send_email(self): 
+    def send_forgot_password_email(self): 
         if database.get_user_email(self.get_email()):
-            sender_email = os.getenv("SENDER_EMAIL")
-            rec_email = "example@email.com"
-            password = os.getenv("PASSWORD_EMAIL")
-            message = f"{self.get_email()} esqueceu a senha!"
+            msg = MIMEMultipart()
+            msg["From"] = self.sender_email
+            msg["To"] = self.receiver_email
+            msg["Subject"] = "Recuperação de senha Enigmarg"
+            message = f"""
+                <h1>Recuperação de senha Enigmarg</h1>
+                <p>Prezado(a) Professor(a),</p>
+                <p>Um dos usuários do Enigmarg solicitou a redefinição de senha de acesso, e estamos aqui para tentar facilitar o processo.</p>
+                <p>Aqui estão os detalhes do usuário e as instruções para ajudá-lo a recuperar sua senha:</p>
+                <p><strong>E-mail do usuário:</strong> {self.get_email()}</p>
+                <p>Para redefinição de senha do usuário, siga os seguintes passos:</p>
+                <ol>
+                    <li>Faça o login no Enigmarg com as credenciais de professor, para que possa visualizar o gerenciador de usuários.</li>
+                    <li>Procure pelo e-mail do usuário que necessita recuperar a senha e selecione-o, ou simplesmente digite o e-mail do usuário no campo de e-mail.</li>
+                    <li>Insira uma nova senha para esse usuário no campo de senha.</li>
+                    <li>Garanta que todas as informações estejam corretas (email do usuário, senha e o cargo).</li>
+                    <li>Aperte no botão de atualizar, e veja se não ocorreu nenhum erro.</li>
+                </ol>
+                <p>Após todo processo, você já poderá comunicar o usuário da melhor forma, informando suas novas credenciais.</p>
+                <p>Agradecemos pela colaboração.</p>
+                <p>Atenciosamente,<br>Equipe Enigmarg</p>
+            """
+            msg.attach(MIMEText(message, "html"))
 
-            server = smtplib.SMTP("smtp.gmail.com", 587)
+            server = smtplib.SMTP(self.smtp_server, self.smtp_port)
             server.starttls()
-            server.login(sender_email, password)
+            server.login(self.sender_email, self.sender_password)
             print("Login feito com sucesso!")
-            server.sendmail(sender_email, rec_email, message)
+            server.sendmail(self.sender_email, self.receiver_email, msg.as_string())
             print("Email enviado com sucesso!")
         else:
             messagebox.showerror("Erro", "Email inválido.")
@@ -109,7 +136,7 @@ class Login():
         self.email_input = customtkinter.CTkEntry(self.screen, width=200, height=30, placeholder_text="E-mail", border_width=0)
         self.email_input.place(relx=0.5, rely=0.5, anchor="center")
 
-        send_btn = customtkinter.CTkButton(self.screen, width=200, height=30, text="Enviar", fg_color="royal blue", hover=False, command=self.send_email, cursor="hand2")
+        send_btn = customtkinter.CTkButton(self.screen, width=200, height=30, text="Enviar", fg_color="royal blue", hover=False, command=self.send_forgot_password_email, cursor="hand2")
         send_btn.place(relx=0.5, rely=0.6, anchor="center")
 
         return_btn = customtkinter.CTkButton(self.screen, width=0, height=0, text="Voltar", command=self.create_login_screen, text_color="white", fg_color="transparent", hover=False, cursor="hand2")
@@ -122,4 +149,3 @@ if __name__ == "__main__":
     database = Database()
     database.connect()
     Login().create_login_screen()
-    # Login().send_email()
